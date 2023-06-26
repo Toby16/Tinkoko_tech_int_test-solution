44df5d354a5da3248a811a2110a4286e1fd21fc5
Toby16 <bolajibaasit14@gmail.com>
2023-06-26 13:59:20 -0400

    /get-user/[:id] (get a user record using the unique id ) | Report: fixed error | created class to convert digital object to float for successful JSON serialization

--- task_3/get_user_with_id.py
+++ task_3/get_user_with_id.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """
-will update later
+Task 3
+Author: Baasit Ayomiposi Bolaji
+
+Lambda function to retrieve user data from a DynamoDB table
+by ID.
 """
 
 import boto3
@@ -12,7 +16,24 @@ import json
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("Tinkoko_table")
 
+
+class DecimalEncoder(json.JSONEncoder):
+    """
+    Custom JSON encoder class that handles Decimal objects/raw data
+    and converts them to a JSON-serializable format.
+
+    This class is created to fix the error where
+    'json.dumps()' function is unable to handle Decimal objects directly.
+    """
+    def default(self, o):
+        # Convert Decimal objects to float for JSON serialization
+        if isinstance(o, Decimal):
+            return float(o)
+        return super(DecimalEncoder, self).default(o)
+
+
 def lambda_handler(event, context):
+    # Extract the user ID from the path parameters of the event
     user_id = event["pathParameters"]["id"]
     # user_id = event["id"]
 
@@ -21,11 +42,13 @@ def lambda_handler(event, context):
     user_data = response.get("Item")
 
     if not user_data:
+        # on error, return 400 status code with error message in response body
         return {
             'statusCode': 400,
             'body': json.dumps({"message": "User not found"})
         }
 
+    # Return a 200 status code with the user data in the response body
     return {
         'statusCode': 200,
         'headers': {
@@ -33,5 +56,5 @@ def lambda_handler(event, context):
         },
         'body': json.dumps({
             'body': user_data
-        })
+        }, cls=DecimalEncoder)
     }
