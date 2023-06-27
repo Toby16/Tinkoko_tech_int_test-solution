a2bd615e232f9f561db4152b9cf46b38d2ed81f0
Toby16 <bolajibaasit14@gmail.com>
2023-06-27 02:06:53 -0400

    /list-product (get list of product; Limits=10) | included 'except' statement for proper error handling | pycodestyle complaint | non-executable

--- task_4/get_list_of_products.py
+++ task_4/get_list_of_products.py
@@ -4,8 +4,8 @@
 """
 import boto3
 import json
-from decimal import Decimal # used to fix "'Decimal' not found" error
-                            # for JSON serialization
+from decimal import Decimal  # used to fix "'Decimal' not found" error
+# for JSON serialization
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Tinkoko_table')
@@ -13,12 +13,24 @@ table = dynamodb.Table('Tinkoko_table')
 # from that single table instead of creating a second table...
 # and filling it with static data
 
+
 def lambda_handler(event, context):
+    """
+    Lambda handler function
+    for retrieving list of products from dynamodb by sellerId.
+    * Args:
+        event (dict): The event data passed to the Lambda function.
+        context (object): The runtime information of the Lambda function.
+    * Returns:
+        dict: The response containing the status code and body.
+    * Raises:
+        KeyError: If any required field is missing in the request payload.
+    """
     # taking the 'try/except' approach
     # for proper error debugging
     try:
         # Get list of products from DynamoDB
-        response = table.scan(Limit=10) # as instructed by the intreviewer
+        response = table.scan(Limit=10)  # as instructed by the intreviewer
         products = response['Items']
 
         # Convert Decimal objects to floats to fix "'Decimal' not found" error
@@ -56,8 +68,15 @@ def lambda_handler(event, context):
             'items': filtered_products
         }
 
-        # Return the response payload
+        # Return the response payload with 200 'successful' status code
         return {
             'statusCode': 200,
             'body': json.dumps(response_payload)
+            # ensures body is JSON serializable
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            # 'products': [product for product in products],
+            'body': json.dumps({'error': str(e)})
         }
